b283b0a4b99e001ba97cab9f67b042e7160f56de
Stephen Kyriacou <Stephen@Unknown-f0-b4-79-21-ca-7a.home>
2015-06-24 00:42:19 +0100

    branch1

--- todo.js
+++ todo.js
@@ -3,21 +3,21 @@ var express = require("express");
 var router = express.Router();
 
 var todoItems = [
-         {id: 1, desc: 'foo'},
-         {id: 2, desc: 'bar'},
-         {id: 3, desc: 'baz'}
+         {id: 1, desc: 'WE'},
+         {id: 2, desc: 'WE'},
+         {id: 3, desc: 'WE'}
          ];
 
 router.get('/', function(req,res){
     res.render('index',{
-      title: 'My Todo List',
+      title: 'WGA',
       items: todoItems
         
     });
 });
 
 router.post('/add', function(req,res){
-	var newItem = req.body.newItem;
+	var newItem = "WE";
 	todoItems.push({
 		id: todoItems.length + 1,
 		desc: newItem
